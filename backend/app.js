9e5b14e04fd9f3450c0645956b20b99e24ed5a13
RISH136 <harsh03092003@gmail.com>
2025-06-05 14:17:19 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+app.use('./',(req,res)=>{
+    res.send('hello');
+})
+
+
+
